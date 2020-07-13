9cbab0383d573f2d2f6763caa7784ebfa4fccb6f
joelramire5 <65499674+joelramire5@users.noreply.github.com>
2020-07-13 00:38:57 -0600

    CargarHash

--- src/com/company/UiHelper/MenuHash.java
+++ src/com/company/UiHelper/MenuHash.java
@@ -1,8 +1,10 @@
 package com.company.UiHelper;
 
+import com.company.Entidades.Nodo;
+import com.company.Entidades.Producto;
 import com.company.HashTable.HashTable;
-import com.company.Logica.cargarhashtable;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Scanner;
@@ -10,7 +12,8 @@ import java.util.Scanner;
 public class MenuHash {
     Scanner input = new Scanner(System.in);
     PrintStream output = new PrintStream(System.out);
-    cargarhashtable cargarhash=new cargarhashtable();
+    private HashTable arraylistproductos = new HashTable(50);
+    private boolean datoscargados=false;
     HashTable hashTable=new HashTable(4096);
 
     public void principal(){
@@ -18,7 +21,7 @@ public class MenuHash {
         System.out.println("Seleccione una opcion del siguiente menu: ");
         System.out.println("Opcion 1: Cargar Datos");
         System.out.println("Opcion 2: Buscar Datos");
-        System.out.println("Opcion 3: Imprimir Codigos");
+        System.out.println("Opcion 3: Imprimir Lista con Posiciones");
         System.out.println("Opcion 4: Salir");
     }
 
@@ -32,15 +35,15 @@ public class MenuHash {
 
             switch (opcion) {
                 case 1:
-                   cargarhash.cargarhashtable();
+                   cargarhashtable();
                     break;
 
                 case 2:
-                    hashTable.obtener();
+                   arraylistproductos.obtener();
                     break;
 
                 case 3:
-                  hashTable.mostrarposicion();
+                 arraylistproductos.mostrarposicion();
                     break;
 
                 case 4:
@@ -53,6 +56,29 @@ public class MenuHash {
 
         } while (opcion != 4 ) ;
     }
+    public void cargarhashtable () throws FileNotFoundException {
+        String Flipkart="C:\\Users\\taera\\IdeaProjects\\TareaProgramada2\\src\\com\\company\\Archivos\\flipkart_com-ecommerce_sample.csv";
+        Scanner lector = new Scanner (new File(Flipkart));
+        int contador=0;
 
+        while (lector.hasNextLine()){
+            String currentline="";
+            currentline=lector.nextLine();
+            if(contador!=0) {
+
+                String[] datos = currentline.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                Producto nuevoProducto = new Producto();
+                nuevoProducto.setCodigo(datos[0]);
+                nuevoProducto.setNombre(datos[3]);
+                nuevoProducto.setCategoria(datos[4]);
+                Nodo nuevonodo = new Nodo(nuevoProducto);
+                arraylistproductos.AgregarHash(nuevonodo);
+            }
+            contador++;
+        }
+        lector.close();
+        datoscargados=true;
+        System.out.println("Se han cargado "+ contador+" datos");
+    }
 
 }
